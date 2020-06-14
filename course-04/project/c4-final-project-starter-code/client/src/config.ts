9fb447e7e349abe1be439e7820492295f7b182a9
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-06-14 12:24:27 +0300

    security changes

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,9 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'at6mudtbg6'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
+
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'rguner.us.auth0.com',
+  clientId: '4O2ObX36y7WImgotJhuV94tY1p7KWEe1',
   callbackUrl: 'http://localhost:3000/callback'
 }
