b58ec3984355d7a431ecd63daae1fe97669d4805
Ramazan Güner <ramazan.guner@turkcell.com.tr>
2020-06-15 21:16:46 +0300

    auth url change

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,9 +1,9 @@
-const apiId = 'at6mudtbg6'
+const apiId = '0udrr1tquc'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 
 export const authConfig = {
-  domain: 'rguner.us.auth0.com',
-  clientId: '4O2ObX36y7WImgotJhuV94tY1p7KWEe1',
+  domain: 'dev-ba5ouksr.us.auth0.com',            
+  clientId: 'uZdg7I7GVcSx0HlUsLKpnlE6NNT15UeN',          
   callbackUrl: 'http://localhost:3000/callback'
 }
